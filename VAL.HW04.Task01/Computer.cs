8a9d7b9b7af7f433d878c77ac791355fe082fa66
vkuzenko <vkuzenko@gmail.com>
2019-03-30 14:34:36 +0200

    HW02, HW04 project folders refactoring.

--- VAL.HW04.Task01/Computer.cs
+++ VAL.HW04.Task01/Computer.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task01
+namespace VAL.HW04.Task01
 {
     public class ComputerNotebook
     {
--- VAL.HW04.Task02/Worker.cs
+++ VAL.HW04.Task02/Worker.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task02
+namespace VAL.HW04.Task02
 {
     class Worker
     {
