a5164b39b3f011dffa00303608fea833aed76c33
vkuzenko <vkuzenko@gmail.com>
2019-05-08 11:12:37 +0300

    Unused usage is removed

--- VAL.HW07.Task01.Logger/IStorage.cs
+++ VAL.HW07.Task01.Logger/IStorage.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VAL.HW07.Task01.Logger
+﻿namespace VAL.HW07.Task01.Logger
 {
     public interface IStorage
     {
--- VAL.HW07.Task01.Logger/StorageFile.cs
+++ VAL.HW07.Task01.Logger/StorageFile.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using System.Threading;
+﻿using System.IO;
 
 namespace VAL.HW07.Task01.Logger
 {
